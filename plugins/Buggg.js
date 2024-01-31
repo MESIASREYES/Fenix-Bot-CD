6c5b5dbf6bd34bcd17ef04c2a6a75c5d317aedc2
REYES <153043081+MESIASREYES@users.noreply.github.com>
2024-01-31 15:53:57 -0600

    Update Buggg.js

--- plugins/Buggg.js
+++ plugins/Buggg.js
@@ -1,4 +1,4 @@
-const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
+1const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 var handler = async(m, { conn, text, usedPrefix, command }) => {
   
  let msg = await generateWAMessageFromContent(m.chat, { locationMessage: {
